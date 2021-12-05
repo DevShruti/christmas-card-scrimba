c37f5f1721c1a2a8f0549d3d2fa229ef80820e52
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-05 03:57:32 -0600

    grretings listed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const greetingDisplay = document.getElementBiId("greeting-display")
+const greetingDisplay = document.getElementBiId("greeting-display");
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", writeGreeting)
+
+const greetings = [
+    "Santa Claus is coming to town!",
+    "We wish you a Merry Christmas!",
+    "Happy holidays!",
+    "Ho, ho, ho! Merry Christmas!",
+    "Jingle all the way!",
+]
+
